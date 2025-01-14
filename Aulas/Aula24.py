797be4c5036d3c0f72e1cefcb96a56fff1885329
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-01-14 19:53:22 -0300

    Métodos para a Estrutura de Dados - Dicionário

---
+++ Aulas/Aula24.py
@@ -0,0 +1,14 @@
+# Métodos úteis para Dicionários
+# .clear() -> Limpa o dicionário
+# .copy() -> Tira uma cópia do dicionário
+# .fromkeys()
+# .get() -> Uma segunda forma de acessar os valores do nosso dicionário
+# .items() -> Retorna uma lista de tuplas 
+# .keys() -> Retorna as chaves do dicionário
+# .pop() -> Remove a chave informada, e se a chave for encontrada ela remove e informa o valor removido
+# .popitem() -> Não é preciso informar a chave, ele vai removendo as chaves
+# .setdefault()
+# .update() 
+# .values() -> Retorna os valores do dicionário
+# .in() -> Verifica se uma chave já existe no dicionário
+# .del() -> Outra forma de tirar um valor do dicionário
